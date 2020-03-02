ee5ccedf4e3fd725b2e88d86288fbc303a368dde
Omar Faruk <40633797+OmarFaruque96@users.noreply.github.com>
2020-03-02 19:36:58 +0600

    full project

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+//initiating jQuery
+	jQuery(function($) {
+		$(document).ready( function() {
+		  //enabling stickUp on the '.navbar-wrapper' class
+		  $('.navbar-wrapper').stickUp();
+		});
+	});
